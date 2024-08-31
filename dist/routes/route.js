1ecb85d0510cb540158a48aeb82a5d105c1cf1a5
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-08-31 12:02:26 +0530

    remove tracked files

--- dist/routes/route.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-const router = Router();
-import { login } from "../controllers/AuthController.js";
-// Auth Routes
-router.post("/auth/login", login);
-export default router;
